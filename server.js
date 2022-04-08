2fd8a6046db0189c27f2cdefdd7bdb8412a09efc
Ayyaz Zafar <contact@ayyaztech.com>
2022-04-08 14:31:40 +0500

    setup deployment

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+
+let app  = express();
+
+
+app.use(express.static(__dirname+'/dist/angulartmp'));
+
+app.get('/*', (req, resp)=>{
+    resp.sendFile(__dirname+'/dist/angulartmp/index.html')
+})
+
+app.listen(1212);
